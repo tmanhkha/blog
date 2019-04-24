88ab1a97535ac63329c77f544b72d8f010f4015a
manh kha <manhkha.thai@gmail.com>
2019-04-25 00:52:06 +0700

    fix UI

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
   def index
-    @posts = Post.all.page(params[:page]).per(5)
+    @posts = Post.order("created_at desc").page(params[:page]).per(2)
   end
 
   def show

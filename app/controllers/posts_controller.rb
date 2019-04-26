7e247940d60b36aff728bad190d429d0ad297ccd
manh kha <manhkha.thai@gmail.com>
2019-04-26 10:17:36 +0700

    handle n + 1 query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
   def index
-    @posts = Post.order("created_at desc").page(params[:page]).per(5)
+    @posts = Post.includes(:comments).order("created_at desc").page(params[:page]).per(5)
   end
 
-  def show
+def show
     @comment = Comment.new
   end
 
